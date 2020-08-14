f25f663ebdac688e12051f9ea03d2d12193caa2f
Beth Olmo <betholmo13@gmail.com>
2020-08-13 21:18:10 -0400

    permissions edits (not correct yet)

--- IssueTracker/Controllers/ProjectsController.cs
+++ IssueTracker/Controllers/ProjectsController.cs
@@ -48,7 +48,7 @@ namespace IssueTracker.Controllers
         }
 
         // GET: Projects/Create
-        [Authorize]
+        [Authorize(Roles = "Admin")]
         public ActionResult Create()
         {
             return View();
