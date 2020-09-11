696b400a12091c641e66a95c3fc763c4f195c070
Beth Olmo <betholmo13@gmail.com>
2020-09-11 01:45:40 -0400

    Finished Styling

--- IssueTracker/Controllers/ManageController.cs
+++ IssueTracker/Controllers/ManageController.cs
@@ -240,7 +240,7 @@ namespace IssueTracker.Controllers
                 {
                     await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
                 }
-                return RedirectToAction("Index", new { Message = ManageMessageId.ChangePasswordSuccess });
+                return RedirectToAction("Index", "Home");
             }
             AddErrors(result);
             return View(model);
